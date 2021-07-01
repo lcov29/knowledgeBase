27f46f20b3b4983001d8cf78c67c4c7cd9a0e584
Lucas Vossen <lucas.vossen@hotmail.de>
2021-07-01 06:41:35 +0200

    change file

--- Programming/database_systems/mariadb_mysql/connectors/java/mysql_connector_java.java
+++ Programming/database_systems/mariadb_mysql/connectors/java/mysql_connector_java.java
@@ -21,29 +21,21 @@ public class mysql_connector_java {
             // The newInstance() call is a workaround for some
             // broken Java implementations
             Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            System.out.println(System.jdbc.drivers);
 
             Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PW);
-            Statement = stmt = con.createStatement();
 
-        } catch (Exception ex) {
-            System.out.println("SQLException: " + ex.getMessage());
-            System.out.println("SQLState: " + ex.getSQLState());
-            System.out.println("VendorError: " + ex.getErrorCode());
-        }
+            // DQL-Statement
+            Statement stmt = con.createStatement();
+            Resultset rs = stmt.executeQuery("Select * from department order by id asc;");
 
-        // execute SQL-Statements
-        Statement stmt = null;
-        ResultSet rs = null;
-
-        try {
-            stmt = con.createStatement();
-            rs = stmt.executeQuery("Select * from department order by id asc;");
+            // iterate through the resultset
             while(rs.next()) {
                 System.out.print("ID: " + rs.getInt("id"));
                 System.out.println(", name: " + rs.getString("name"));
             }
-        } catch (SQLException ex) {
-            // handle any errors
+
+        } catch (Exception ex) {
             System.out.println("SQLException: " + ex.getMessage());
             System.out.println("SQLState: " + ex.getSQLState());
             System.out.println("VendorError: " + ex.getErrorCode());
